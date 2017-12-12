6b4a2d42a6fceee915a8181f05bf36a123c147a5
AnkitDroidGit <urmailing2ankit@gmail.com>
2017-12-12 11:18:07 +0800

    -added take operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/OperatorsActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/OperatorsActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.freeankit.rxjava2samples.R
-import com.freeankit.rxjava2samples.ui.operators.DisposableOperatorActivity
-import com.freeankit.rxjava2samples.ui.operators.MapOperatorActivity
-import com.freeankit.rxjava2samples.ui.operators.SimpleOperatorActivity
-import com.freeankit.rxjava2samples.ui.operators.ZipOperatorActivity
+import com.freeankit.rxjava2samples.ui.operators.*
 
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 08/12/2017 (MM/DD/YYYY )
@@ -33,5 +30,9 @@ class OperatorsActivity : AppCompatActivity() {
 
     fun startDisposableActivity(view: View) {
         startActivity(Intent(this@OperatorsActivity, DisposableOperatorActivity::class.java))
+    }
+
+    fun startTakeActivity(view: View) {
+        startActivity(Intent(this@OperatorsActivity, TakeOperatorActivity::class.java))
     }
 }
