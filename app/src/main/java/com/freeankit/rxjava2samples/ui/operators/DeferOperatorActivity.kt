e8429d924d8e3e57865e280acb329af4f4f7d11b
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-11 10:49:02 +0800

    Added Defer Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/DeferOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/DeferOperatorActivity.kt
@@ -1,9 +1,66 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.freeankit.rxjava2samples.R
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_example_operator.*
 
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 11/01/2018 (MM/DD/YYYY )
  */
 class DeferOperatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_operator)
+
+        btn.setOnClickListener({ executeDeferOperator() })
+    }
+
+
+    /*
+   * Defer used for Deferring Observable code until subscription in RxJava
+   */
+    private fun executeDeferOperator() {
+        val car = Car()
+
+        val brandDeferObservable = car.brandDeferObservable()
+
+        car.setBrand("BMW")  // Even if we are setting the brand after creating Observable
+        // we will get the brand as BMW.
+        // If we had not used defer, we would have got null as the brand.
+
+        brandDeferObservable
+                .subscribe(getObserver())
+
+    }
+
+    private fun getObserver(): Observer<String> {
+        return object : Observer<String> {
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed)
+            }
+
+            override fun onNext(value: String) {
+                textView.append(" onNext : value : " + value)
+                textView.append(AppConstant.LINE_SEPARATOR)
+                Log.d(TAG, " onNext : value : " + value)
+            }
+
+            override fun onError(e: Throwable) {
+                textView.append(" onError : " + e.message)
+                textView.append(AppConstant.LINE_SEPARATOR)
+                Log.d(TAG, " onError : " + e.message)
+            }
+
+            override fun onComplete() {
+                textView.append(" onComplete")
+                textView.append(AppConstant.LINE_SEPARATOR)
+                Log.d(TAG, " onComplete")
+            }
+        }
+    }
 }
