5c9b64ea7eb8f873524f8634f90ab21e8f0e3dde
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-25 15:04:16 +0800

    Added Distinct Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/DistinctOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/DistinctOperatorActivity.kt
@@ -2,7 +2,12 @@ package com.freeankit.rxjava2samples.ui.operators
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.freeankit.rxjava2samples.R
+import com.freeankit.rxjava2samples.utils.Constant
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_example_operator.*
 
 /**
@@ -21,6 +26,36 @@ class DistinctOperatorActivity : AppCompatActivity() {
     */
     private fun executeDistinctOperator() {
 
+        getObservable()
+                .distinct()
+                .subscribe(getObserver())
+    }
 
+    private fun getObservable(): Observable<Int> {
+        return Observable.just(1, 2, 1, 1, 2, 3, 4, 6, 4)
+    }
+
+
+    private fun getObserver(): Observer<Int> {
+        return object : Observer<Int> {
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(Constant().TAG, " onSubscribe : " + d.isDisposed)
+            }
+
+            override fun onNext(value: Int) {
+                textView.append(" onNext : value : " + value!!)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " onNext value : " + value)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(Constant().TAG, " onError : " + e.message)
+            }
+
+            override fun onComplete() {
+                Log.d(Constant().TAG, " onComplete")
+            }
+        }
     }
 }
