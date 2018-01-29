3832d2495fc6999598be68b1d1d622bbb43d1370
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-29 10:53:09 +0800

    Added Replay Subject Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
@@ -2,7 +2,11 @@ package com.freeankit.rxjava2samples.ui.operators
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.freeankit.rxjava2samples.R
+import com.freeankit.rxjava2samples.utils.Constant
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.ReplaySubject
 import kotlinx.android.synthetic.main.activity_example_operator.*
 
@@ -36,5 +40,61 @@ class ReplaySubjectOperatorActivity : AppCompatActivity() {
          */
         source.subscribe(getSecondObserver())
 
+    }
+
+    private fun getFirstObserver(): Observer<Int> {
+        return object : Observer<Int> {
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(Constant().TAG, " First onSubscribe : " + d.isDisposed)
+            }
+
+            override fun onNext(value: Int) {
+                textView.append(" First onNext : value : " + value!!)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onNext value : " + value)
+            }
+
+            override fun onError(e: Throwable) {
+                textView.append(" First onError : " + e.message)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onError : " + e.message)
+            }
+
+            override fun onComplete() {
+                textView.append(" First onComplete")
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onComplete")
+            }
+        }
+    }
+
+    private fun getSecondObserver(): Observer<Int> {
+        return object : Observer<Int> {
+
+            override fun onSubscribe(d: Disposable) {
+                textView.append(" Second onSubscribe : isDisposed :" + d.isDisposed)
+                Log.d(Constant().TAG, " Second onSubscribe : " + d.isDisposed)
+                textView.append(Constant().LINE_SEPARATOR)
+            }
+
+            override fun onNext(value: Int) {
+                textView.append(" Second onNext : value : " + value!!)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onNext value : " + value)
+            }
+
+            override fun onError(e: Throwable) {
+                textView.append(" Second onError : " + e.message)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onError : " + e.message)
+            }
+
+            override fun onComplete() {
+                textView.append(" Second onComplete")
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onComplete")
+            }
+        }
     }
 }
