a40a461a36e6cee17fecfd10a1e69e8faddf856e
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-29 11:09:11 +0800

    Added Publish Subject Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/PublishSubjectOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/PublishSubjectOperatorActivity.kt
@@ -1,7 +1,23 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.freeankit.rxjava2samples.R
+import kotlinx.android.synthetic.main.activity_example_operator.*
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 29/01/2018 (MM/DD/YYYY )
  */
-class PublishSubjectOperatorActivity {
+class PublishSubjectOperatorActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_operator)
+
+        btn.setOnClickListener({ executePublishSubjectOperator() })
+    }
+
+    private fun executePublishSubjectOperator() {
+
+
+    }
 }
