e85116bcebdadea9e200e126507420f87d76505e
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-29 10:48:02 +0800

    Added Replay Subject Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
@@ -1,7 +1,9 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.support.v7.app.AppCompatActivity
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 29/01/2018 (MM/DD/YYYY )
  */
-class ReplaySubjectOperatorActivity {
+class ReplaySubjectOperatorActivity : AppCompatActivity() {
 }
