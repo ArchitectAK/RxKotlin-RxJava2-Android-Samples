876cdd7ab982c4b5922c625b86beda64e7a59bda
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-29 10:50:40 +0800

    Added Replay Subject Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ReplaySubjectOperatorActivity.kt
@@ -1,9 +1,26 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.freeankit.rxjava2samples.R
+import kotlinx.android.synthetic.main.activity_example_operator.*
 
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 29/01/2018 (MM/DD/YYYY )
  */
 class ReplaySubjectOperatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_operator)
+
+        btn.setOnClickListener({ executeReplaySubjectOperator() })
+    }
+
+    /* ReplaySubject emits to any observer all of the items that were emitted
+    * by the source Observable, regardless of when the observer subscribes.
+    */
+    private fun executeReplaySubjectOperator() {
+
+
+    }
 }
