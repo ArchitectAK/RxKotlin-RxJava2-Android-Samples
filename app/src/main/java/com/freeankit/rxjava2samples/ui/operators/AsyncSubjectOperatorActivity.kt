c84a1ef11f431b91d7c46c288449cefd0f45153c
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-02-08 10:12:16 +0800

    Added Async Subject Sample

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/AsyncSubjectOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/AsyncSubjectOperatorActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.freeankit.rxjava2samples.R
+import com.freeankit.rxjava2samples.utils.Constant
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.AsyncSubject
@@ -48,25 +49,25 @@ class AsyncSubjectOperatorActivity :AppCompatActivity(){
         return object : Observer<Int> {
 
             override fun onSubscribe(d: Disposable) {
-                Log.d(TAG, " First onSubscribe : " + d.isDisposed)
+                Log.d(Constant().TAG, " First onSubscribe : " + d.isDisposed)
             }
 
-            override fun onNext(value: Int?) {
+            override fun onNext(value: Int) {
                 textView.append(" First onNext : value : " + value!!)
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " First onNext value : " + value)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onNext value : " + value)
             }
 
             override fun onError(e: Throwable) {
                 textView.append(" First onError : " + e.message)
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " First onError : " + e.message)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onError : " + e.message)
             }
 
             override fun onComplete() {
                 textView.append(" First onComplete")
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " First onComplete")
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " First onComplete")
             }
         }
     }
@@ -76,26 +77,26 @@ class AsyncSubjectOperatorActivity :AppCompatActivity(){
 
             override fun onSubscribe(d: Disposable) {
                 textView.append(" Second onSubscribe : isDisposed :" + d.isDisposed)
-                Log.d(TAG, " Second onSubscribe : " + d.isDisposed)
-                textView.append(AppConstant.LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onSubscribe : " + d.isDisposed)
+                textView.append(Constant().LINE_SEPARATOR)
             }
 
-            override fun onNext(value: Int?) {
+            override fun onNext(value: Int) {
                 textView.append(" Second onNext : value : " + value!!)
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " Second onNext value : " + value)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onNext value : " + value)
             }
 
             override fun onError(e: Throwable) {
                 textView.append(" Second onError : " + e.message)
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " Second onError : " + e.message)
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onError : " + e.message)
             }
 
             override fun onComplete() {
                 textView.append(" Second onComplete")
-                textView.append(AppConstant.LINE_SEPARATOR)
-                Log.d(TAG, " Second onComplete")
+                textView.append(Constant().LINE_SEPARATOR)
+                Log.d(Constant().TAG, " Second onComplete")
             }
         }
     }
