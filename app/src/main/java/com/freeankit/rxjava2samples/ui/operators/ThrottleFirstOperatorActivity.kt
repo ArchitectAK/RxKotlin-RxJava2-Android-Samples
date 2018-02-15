473b5746dc6bdf445676363397e3676bb792cfb8
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-02-15 10:22:34 +0800

    Added Throttle First Sample

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ThrottleFirstOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ThrottleFirstOperatorActivity.kt
@@ -1,7 +1,23 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.freeankit.rxjava2samples.R
+import kotlinx.android.synthetic.main.activity_example_operator.*
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 15/02/2018 (MM/DD/YYYY )
  */
-class ThrottleFirstOperatorActivity {
+class ThrottleFirstOperatorActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_operator)
+
+        btn.setOnClickListener({ executeThrottleFirstOperator() })
+    }
+
+    private fun executeThrottleFirstOperator() {
+
+
+    }
 }
