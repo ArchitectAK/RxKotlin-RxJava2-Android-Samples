261718e5b386baffc71ddd979a9da9f1e15cbf21
AnkitDroidGit <Ankit_007>
2018-02-17 20:34:08 +0800

    Add Throttle Last Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ThrottleLastOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/ThrottleLastOperatorActivity.kt
@@ -1,7 +1,9 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.support.v7.app.AppCompatActivity
+
 /**
  *@author by Ankit Kumar (ankitdroiddeveloper@gmail.com) on 2/17/18 (MM/DD/YYYY )
  **/
-class ThrottleLastOperatorActivity {
+class ThrottleLastOperatorActivity : AppCompatActivity() {
 }
