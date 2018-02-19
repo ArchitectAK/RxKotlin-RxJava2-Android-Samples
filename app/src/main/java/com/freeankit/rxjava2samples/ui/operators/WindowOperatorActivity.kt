730d89d92ba330767815cab409626079b78a8e00
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-02-19 10:06:37 +0800

    Added Window Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/WindowOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/WindowOperatorActivity.kt
@@ -1,7 +1,31 @@
 package com.freeankit.rxjava2samples.ui.operators
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.freeankit.rxjava2samples.R
+import kotlinx.android.synthetic.main.activity_example_operator.*
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 19/02/2018 (MM/DD/YYYY )
  */
-class WindowOperatorActivity {
+class WindowOperatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_operator)
+
+        btn.setOnClickListener({ executeWindowOperator() })
+    }
+
+
+    /*
+    * Example using window operator -> It periodically
+    * subdivide items from an Observable into
+    * Observable windows and emit these windows rather than
+    * emitting the items one at a time
+    */
+
+    private fun executeWindowOperator() {
+
+
+    }
 }
