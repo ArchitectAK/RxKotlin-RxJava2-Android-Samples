d6b194fcca117b3646e577bef51dec7588d021f3
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-02-19 10:07:41 +0800

    Added Window Operator

--- app/src/main/java/com/freeankit/rxjava2samples/ui/operators/WindowOperatorActivity.kt
+++ app/src/main/java/com/freeankit/rxjava2samples/ui/operators/WindowOperatorActivity.kt
@@ -2,8 +2,15 @@ package com.freeankit.rxjava2samples.ui.operators
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.freeankit.rxjava2samples.R
+import com.freeankit.rxjava2samples.utils.Constant
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_example_operator.*
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 19/02/2018 (MM/DD/YYYY )
@@ -25,7 +32,28 @@ class WindowOperatorActivity : AppCompatActivity() {
     */
 
     private fun executeWindowOperator() {
+        Observable.interval(1, TimeUnit.SECONDS).take(12)
+                .window(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getConsumer())
+    }
 
-
+    fun getConsumer(): Consumer<Observable<Long>> {
+        return Consumer { observable ->
+            Log.d(Constant().TAG, "Sub Divide begin....")
+            textView.append("Sub Divide begin ....")
+            textView.append(Constant().LINE_SEPARATOR)
+            observable
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(object : Consumer<Long> {
+                        override fun accept(value: Long) {
+                            Log.d(Constant().TAG, "Next:" + value)
+                            textView.append("Next:" + value)
+                            textView.append(Constant().LINE_SEPARATOR)
+                        }
+                    })
+        }
     }
 }
